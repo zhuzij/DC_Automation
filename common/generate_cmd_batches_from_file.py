eb38f13d129ff41b6b285ae4cf137c9122c468e0
dashiawia <yuzhengzhu@gmail.com>
2023-10-23 13:50:36 -0400

    updated run fgt cli.py

---
+++ common/generate_cmd_batches_from_file.py
@@ -0,0 +1,29 @@
+def read_command_batches_from_file(file_path):
+    """
+    Read the FortiGate policy configurations from a text file and split them into command batches
+    using the keyword 'next' as a delimiter.
+
+    :param file_path: Path to the text file containing the commands
+    :return: List of command batches; each batch is a list of commands for the same policy ID
+    """
+    command_batches = []
+    command_batch = []
+
+    # Open the text file and read line by line
+    with open(file_path, 'r') as f:
+        for line in f:
+            line = line.strip()  # Remove leading/trailing white spaces
+            if line:  # Skip empty lines
+                command_batch.append(line)
+                if line == 'next':  # Cut at 'next' keyword
+                    cmd_batch = '\n'.join(command_batch)
+                    # command_batches.append(command_batch.copy())
+                    command_batches.append(cmd_batch)
+                    command_batch.clear()
+
+    return command_batches
+
+# Example usage
+file_path = "C:/Users/jacki/Downloads/labfgt_pol_config.txt"  # Replace this with the path to your file
+command_batches = read_command_batches_from_file(file_path)
+print(command_batches)
--- fortigate/fortigate_tools/run_commands_FGT_v1_OOP copy.py
+++ fortigate/fortigate_tools/run_commands_FGT_v1_OOP copy.py
@@ -7,25 +7,12 @@ logging.basicConfig(level=logging.DEBUG)
 import os
 import sys
 sys.path.append(os.getcwd())
-
-# Assuming these imports match with your own modules and logic
 from common.timeit import timeit
-from common.generate_cmd_batches_from_file import command_batches  # Assuming commands_string is a list of command batches
+from fortigate.fortigate_tools.generate_commands import commands_string
 
 
 class FortiGateCLIAsync:
-    """
-    Asynchronous SSH client for interacting with FortiGate devices.
-    """
     def __init__(self, host, username, password, timeout=10):
-        """
-        Initialize connection settings and SSH client.
-
-        :param host: IP address of the FortiGate device
-        :param username: SSH username
-        :param password: SSH password
-        :param timeout: Timeout for the SSH connection, in seconds
-        """
         self.host = host
         self.username = username
         self.password = password
@@ -37,9 +24,6 @@ class FortiGateCLIAsync:
         self.is_connected = False  # Initialize to False
 
     async def connect(self):
-        """
-        Asynchronously establish an SSH connection using ThreadPoolExecutor.
-        """
         self.loop = asyncio.get_running_loop()
         try:
             def wrapper():
@@ -57,60 +41,36 @@ class FortiGateCLIAsync:
             self.is_connected = False
             print(f"Failed to connect due to exception: {e}")
 
-    def _exec_command(self, ssh_client, command):
-        """
-        Execute a single command over SSH and return the output.
 
-        :param ssh_client: Active Paramiko SSH client
-        :param command: Command to run
-        :return: Command output as a string
-        """
+    def _exec_command(self, ssh_client, command):
         stdin, stdout, stderr = ssh_client.exec_command(command)
         return stdout.read().decode('utf-8')
 
-    async def run_command_batch(self, command_batch):
-        """
-        Asynchronously execute a batch of commands over SSH.
-
-        :param command_batch: List of commands to run sequentially
-        :return: Concatenated output from all commands
-        """
+    async def run_command(self, command):
         if self.loop is None:
             self.loop = asyncio.get_running_loop()
         if not self.is_connected:
             print("Not connected. Please connect first.")
             return
         try:
-            output = ''
-            for cmd in command_batch:
-                output += await self.loop.run_in_executor(self.executor, self._exec_command, self.ssh_client, cmd)
+            output = await self.loop.run_in_executor(self.executor, self._exec_command, self.ssh_client, command)
             return output
         except Exception as e:
-            print(f"Failed to run command batch due to exception: {e}")
+            print(f"Failed to run command due to exception: {e}")
+
 
     async def disconnect(self):
-        """
-        Asynchronously close the SSH connection.
-        """
         if self.loop is None:
             self.loop = asyncio.get_running_loop()
         await self.loop.run_in_executor(self.executor, self.ssh_client.close)
 
-
 # Example usage
 @timeit
 async def main():
-    """
-    Example asynchronous routine to connect, send command batches, and disconnect.
-    """
-    fgt_cli = FortiGateCLIAsync(host="192.168.3.1", username="joe", password="Iching12#")
+    fgt_cli = FortiGateCLIAsync(host="192.168.3.1", username="joe", password="x")
     await fgt_cli.connect()
-    
-    # Assuming commands_string is a list of command batches; each batch is a list of commands for the same policy ID
-    for command_batch in command_batches:
-        output = await fgt_cli.run_command_batch(command_batch)
-        print(output)
-    
+    output = await fgt_cli.run_command(f"{commands_string}")
+    print(output)
     await fgt_cli.disconnect()
 
 if __name__ == "__main__":
